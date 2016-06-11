4bdd8c8a568678943f9c33261364c6d462755609
Dominic Burkart <1dominicb@gmail.com>
2016-06-11 17:44:45 -0400

    building request!

--- grindrQuery.js
+++ grindrQuery.js
@@ -1,7 +1,26 @@
-
-
 /* 1.) get current location*/
 
+/*
+from: http://www.w3schools.com/html/html5_geolocation.asp
+*/
+function getLocation() {
+    if (navigator.geolocation) {
+        var pos = navigator.geolocation.getCurrentPosition(showPosition);
+        return [pos.coords.latitude, pos.coords.longitude];
+    } else {
+        return null; //location is not supported
+    }
+}
+
+var pos = getLocation();
+
+if (pos === null){
+  pos = [40.712784, -74.005941];
+}
+
+console.log(pos);
 /* 2.) create JSON request object */
+
+
 
 /* 3.) send request object and recieve data */
