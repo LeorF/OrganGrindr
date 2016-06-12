1baa1adb5d0d52fb7346a3a694a4ecf070810997
LeorF <lf1170@nyu.edu>
2016-06-11 20:31:04 -0400

    workinggggg

--- grindrQuery.js
+++ grindrQuery.js
@@ -1,8 +1,71 @@
+var https = require('https');
+var express = require('express');
+var app = express();
+var path = require("path");
+
+var publicPath = path.resolve(__dirname, "views");
+app.use(express.static(publicPath));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var handlebars = require('express-handlebars') 
+	.create({ defaultLayout:'main' });
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.post('/', function(request, response) {
+	console.log("posted!");
+	var querystring = require('querystring');
+	var data = JSON.stringify({
+		"filter": {
+			"onlineOnly": true, 
+			"page": 1, 
+			"quantity": 150
+		}, 
+		"lat": pos[0],
+		"lon": pos[1]
+	});
+
+	var options = {
+		host: 'primus.grindr.com',
+		port: 443,
+		path: '/2.0/nearbyProfiles',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	var req = https.request(options, function(res) {
+		console.log('Status: ' + res.statusCode);
+		console.log('Headers: ' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		res.on('data', function (body) {
+		console.log('Body: ' + body);
+		});
+	});
+
+	req.write(data);
+	req.end();
+});
+
 /* 1.) get current location*/
 
 /*
 from: http://www.w3schools.com/html/html5_geolocation.asp
 */
+
+/*
+Stretch goal: Make this work lol
 function getLocation() {
     if (navigator.geolocation) {
         var pos = navigator.geolocation.getCurrentPosition(showPosition);
@@ -14,13 +77,50 @@ function getLocation() {
 
 var pos = getLocation();
 
-if (pos === null){
-  pos = [40.712784, -74.005941];
-}
+if (pos === null){*/
+
+var pos = [40.712784, -74.005941];
+
 
 console.log(pos);
-/* 2.) create JSON request object */
+/*
+2.) create JSON request object */
+/*
+reference: https://github.com/tomlandia/fuckr/blob/master/unofficial-grindr-api-documentation.md
+*/
 
 
+/*
+var req = new XMLHttpRequest();
+req.open("POST", "https://primus.grindr.com/2.0/nearbyProfiles");
+req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+var res = req.send(reqString);
+console.log(res);
+*/
 
 /* 3.) send request object and recieve data */
+
+
+app.listen(4000);
+console.log('Started server on port 4000');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
